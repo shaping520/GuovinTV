f009f3f988449fa92036f84faecb8eeab058d260
shaping520 <144744478+shaping520@users.noreply.github.com>
2024-06-17 13:43:23 +0800

    Create user_config.py

---
+++ user_config.py
@@ -0,0 +1,31 @@
+source_file = "user_demo.txt"
+final_file = "user_result.txt"
+favorite_list = [
+    "广东珠江",
+    "CCTV-1",
+    "CCTV-5",
+    "CCTV-5+",
+    "CCTV-13",
+    "广东体育",
+    "广东卫视",
+    "大湾区卫视",
+    "浙江卫视",
+    "湖南卫视",
+    "翡翠台",
+]
+open_online_search = False
+favorite_page_num = 5
+default_page_num = 3
+urls_limit = 15
+open_sort = True
+response_time_weight = 0.5
+resolution_weight = 0.5
+recent_days = 30
+ipv_type = "ipv4"
+domain_blacklist = ["epg.pw"]
+url_keywords_blacklist = []
+open_subscribe = True
+subscribe_urls = [
+]
+open_multicast = True
+region_list = ["all"]
